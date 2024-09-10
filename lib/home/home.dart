195d3ecb9c378f9e20c04eb612cfec2c2f4ddc87
Praveen Chandra <pcps72001@gmail.com>
2024-09-10 20:56:19 +0530

    final done

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -21,51 +21,16 @@ class HomeView extends StatelessWidget {
       backgroundColor: white,
       key: scaffoldKey,
       appBar: AppBar(
-        toolbarHeight: 120,
         backgroundColor: white,
         automaticallyImplyLeading: false,
-        title: Padding(
-          padding: const EdgeInsets.all(18.0), // Add 20 pixel top padding
-          child: Column(
-            children: [
-              gapH(20),
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage(ImagePath.dp),
-                  ),
-                  SizedBox(width: 15),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      customText('Welcome', lightBlack, 20.0, FontWeight.bold),
-                      customText(
-                        'Emily Cyrus',
-                        const Color.fromARGB(255, 234, 111, 164),
-                        20.0,
-                        FontWeight.bold,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
         actions: [
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 2, 18, 0),
-            child: Column(
-              children: [
-                IconButton(
-                  icon: SvgPicture.asset(ImagePath.menu),
-                  onPressed: () {
-                    scaffoldKey.currentState!.openDrawer();
-                  },
-                ),
-                gapH(10),
-              ],
+            child: IconButton(
+              icon: SvgPicture.asset(ImagePath.menu),
+              onPressed: () {
+                scaffoldKey.currentState!.openDrawer();
+              },
             ),
           ),
         ],
@@ -77,16 +42,39 @@ class HomeView extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage(ImagePath.dp),
+                    ),
+                    const SizedBox(width: 15),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        customText('Welcome', lightBlack, 20.0, FontWeight.bold),
+                        customText(
+                          'Emily Cyrus',
+                          const Color.fromARGB(255, 234, 111, 164),
+                          20.0,
+                          FontWeight.bold,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: Stack(
-
                     children: [
                       Column(
                         children: [
                           gapH(40),
                           Container(
                             height: 160,
-                            width: MediaQuery.of(context).size.width / 1.2,
+                            width: MediaQuery.of(context).size.width / 1.25,
                             padding: const EdgeInsets.all(16.0),
                             decoration: BoxDecoration(
                               color: lightPink,
@@ -133,18 +121,17 @@ class HomeView extends StatelessWidget {
                     ],
                   )),
               Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: customText('Your Current Booking', blue, 20.0, FontWeight.bold)
-              ),
+                  padding: const EdgeInsets.all(16.0),
+                  child: customText(
+                      'Your Current Booking', blue, 20.0, FontWeight.bold)),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 18.0),
                 child: CurrentBookingCard(),
               ),
               gapH(26),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: customText("Packages", blue, 20.0, FontWeight.bold)
-              ),
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: customText("Packages", blue, 20.0, FontWeight.bold)),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: PackageListView(),
@@ -161,14 +148,6 @@ class HomeView extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        // Blue line at the top
-        // Padding(
-        //   padding: const EdgeInsets.all(18.0),
-        //   child: Container(
-        //     height: 2, // Adjust the height as needed
-        //     color: Colors.blue, // Blue color for the line
-        //   ),
-        // ),
         BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           backgroundColor: Colors.white,
@@ -183,8 +162,8 @@ class HomeView extends StatelessWidget {
                 height: 24,
                 width: 24,
                 color: controller.selectedCategory.value == 0
-                    ? Color.fromARGB(255, 237, 76, 145) // pink for selected
-                    : Colors.black54, // grey for unselected
+                    ? const Color.fromARGB(255, 237, 76, 145)
+                    : Colors.black54,
               ),
               label: 'Home',
             ),
@@ -194,7 +173,7 @@ class HomeView extends StatelessWidget {
                 height: 24,
                 width: 24,
                 color: controller.selectedCategory.value == 1
-                    ? Color.fromARGB(255, 237, 76, 145)
+                    ? const Color.fromARGB(255, 237, 76, 145)
                     : Colors.black54,
               ),
               label: 'Packages',
@@ -205,7 +184,7 @@ class HomeView extends StatelessWidget {
                 height: 24,
                 width: 24,
                 color: controller.selectedCategory.value == 2
-                    ? Color.fromARGB(255, 237, 76, 145)
+                    ? const Color.fromARGB(255, 237, 76, 145)
                     : Colors.black54,
               ),
               label: 'Bookings',
@@ -216,17 +195,17 @@ class HomeView extends StatelessWidget {
                 height: 24,
                 width: 24,
                 color: controller.selectedCategory.value == 3
-                    ? Color.fromARGB(255, 237, 76, 145)
+                    ? const Color.fromARGB(255, 237, 76, 145)
                     : Colors.black54,
               ),
               label: 'Profile',
             ),
           ],
           selectedItemColor:
-              Color.fromARGB(255, 237, 76, 145), // pink for selected
-          unselectedItemColor: Colors.black54, // grey for unselected
-          showSelectedLabels: true, // Show selected labels
-          showUnselectedLabels: true, // Show unselected labels
+              const Color.fromARGB(255, 237, 76, 145), 
+          unselectedItemColor: Colors.black54, 
+          showSelectedLabels: true, 
+          showUnselectedLabels: true,
         ),
       ],
     );
